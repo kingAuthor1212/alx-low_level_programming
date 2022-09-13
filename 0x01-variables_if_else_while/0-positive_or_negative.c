1f60afc2dcbe930740dc0616b7410c80d66de049
kingAutgor1212 <kingauthor1212@gmail.com>
2022-09-13 03:30:32 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,8 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers  goes there */ 
-#include <stdio.h>
 /**
- * main - print if the number is postive, zero, negative 
- * Description: using the main function 
- * this program prints  is postive, zero, negative 
- * return: 0 
+ * main - print if the number is postive, zero, negative
+ * Description: using the main function
+ * this program prints  is postive, zero, negative
+ * return: 0
  */
 int main(void)
 {
@@ -16,12 +12,13 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if (n > 0)	
-	printf("%! is positive\n", n);
+if (n > 0)
+        printf("%! is positive\n", n);
 else if (n == 0)
-	printf("%! is zero\n", n);
+        printf("%! is zero\n", n);
 else if (n < 0)
-	printf("%! is negative\n", n);
+        printf("%! is negative\n", n);
 return (0);
 }
+
 
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,25 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n;
-int i;
-char str[] = "Last digit of";
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-i = n % 10;
-if(i > 5)
-{
-printf ("%s  %d is %d and is greater than 5\n", str, n, i);   
- }
-else if(i == 0)
-{
-printf("%s %d is %d and is 0\n", str, n, i
-}
-else if(i < 6)
-{
-printf("%s %d is %d and is less than 6 and not 0\n", str, n, i)
-}
-return (0);
+	int n;
+	int i;
+	char str[] = "Last digit of";
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	i = n % 10;
+	if (i > 5)
+		printf ("%s  %d is %d and is greater than 5\n", str, n, i);   
+	else if (i == 0)
+		printf("%s %d is %d and is 0\n", str, n, i);
+	else if (i < 6)
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, i);
+	return (0);
 }
