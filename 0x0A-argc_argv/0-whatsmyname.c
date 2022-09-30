5118aafe1472b8a7a8ae30a64535fbfc7030d4d4
kingAutgor1212 <kingauthor1212@gmail.com>
2022-09-30 14:00:27 -0700

    a program that prints the minimum number of coins to make change for an amount of money.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name, followed by a new line.
+ * @argc: the number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+0. It aint what they call you, its what you answer to
+1. Silence is argument carried out by other means
+2. The best argument against democracy is a five-minute conversation with the average voter
+3. Neither irony nor sarcasm is argument
+4. To infinity and beyond
+5. Minimal Number of Coins for Change
