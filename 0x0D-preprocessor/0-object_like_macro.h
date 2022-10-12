1e7cbfdc277d26fc5eabd74f514147dfafe7b437
kingAutgor1212 <kingauthor1212@gmail.com>
2022-10-12 02:57:19 -0700

    SUM

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#define SIZE_H
+#ifndef SIZE_H
+
+/**
+ * file: 0-object-like-macro.h
+ *
+ * Desc: Header file that defines a macro named
+ * 	 SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,5 @@
+0. Object-like Macro
+1.PI
+2.Filename
+function-like macro
+SUM
