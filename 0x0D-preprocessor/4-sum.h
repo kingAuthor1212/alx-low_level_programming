1d757571abd0f069d2eef15fe828c337af3b88f1
kingAutgor1212 <kingauthor1212@gmail.com>
2022-10-12 03:01:14 -0700

    SUM

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -2,8 +2,7 @@
 #ifndef SUM_H
 
 /*
- * File: 4-sum.h
- * 
+ * File: 4-sum.h 
  * Desc: Header file that defines a function-like macro SUM(x, y)
  *       that computes the sum of the numbers x and y.
  */
