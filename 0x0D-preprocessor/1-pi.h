3ced818f123d1709d95d35862b31423be890165f
kingAutgor1212 <kingauthor1212@gmail.com>
2022-10-12 03:32:43 -0700

    sum

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,9 +1,9 @@
-#define PI_H
 #ifndef PI_H
+#define PI_H
 
 /*
  * File: 1-pi.h
- * 
+ * Auth: kingauthor
  * Desc: Header file that defines a macro named PI
  *       as an abbreviation for the token 3.14159265359.
  */
